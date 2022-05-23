20484aa3c4a56738b096daad6fabddbdbf8eed63
Dan Forder <danfrdr@gmail.com>
2022-05-23 10:15:56 +0100

    Add db packages

--- src/main/java/com/example/gcpDemo/GcpDemoApplication.java
+++ src/main/java/com/example/gcpDemo/GcpDemoApplication.java
@@ -2,10 +2,9 @@ package com.example.gcpDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GcpDemoApplication {
 
 	public static void main(String[] args) {
